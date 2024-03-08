dd907691ed679e760ed55901aba2a51e3a953685
S Varun <varunshanker1718@gmail.com>
2024-03-08 19:41:33 +0530

    render content in course

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Part } from "./Part";
 
-export const Content = ({ courses }) => {
+export const Content = ({ content }) => {
   return (
     <div>
-      <Part part={courses[0].name} exercises={courses[0].exercises} />
-      <Part part={courses[1].name} exercises={courses[1].exercises} />
-      <Part part={courses[2].name} exercises={courses[2].exercises} />
+      <Part part={content[0].name} exercises={content[0].exercises} />
+      <Part part={content[1].name} exercises={content[1].exercises} />
+      <Part part={content[2].name} exercises={content[2].exercises} />
     </div>
   );
 };
--- part1/src/components/Course.jsx
+++ part1/src/components/Course.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "./Header";
+import { Content } from "./Content";
 
 export const Course = ({ course }) => {
   return (
     <div>
       <Header title={course.name} />
+      <Content content={course.parts} />
     </div>
   );
 };
